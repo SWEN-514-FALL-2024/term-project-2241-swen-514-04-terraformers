9ae1684d0677c3f7c455ab63731b2fd1772fad98
Rhamsez Thevenin <rhamzthev@gmail.com>
2024-10-26 10:53:43 -0400

    added content type

--- tf/main.tf
+++ tf/main.tf
@@ -95,6 +95,7 @@ resource "aws_api_gateway_resource" "url_resource" {
 resource "aws_api_gateway_method" "method" {
   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
   resource_id   = aws_api_gateway_resource.url_resource.id
+  request_models = "application/json"
   http_method   = "POST"
   authorization = "NONE"
 }
